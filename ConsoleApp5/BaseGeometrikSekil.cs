dfe086f8f0fc6cad464ae3647ffa98682e624e47
ceyda.yilmaz <ceyda.yilmaz@CEYDAYILMAZPC>
2024-08-14 17:22:59 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp5/BaseGeometrikSekil.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public abstract class BaseGeometrikSekil
+    {
+        public double Genislik { get; set; }
+        public double Yukseklik { get; set; }
+
+        // Her geometrik şeklin kendi alan hesaplaması olacak
+        public abstract double AlanHesapla();
+    }
+
+    public class Kare : BaseGeometrikSekil
+    {
+        // Karenin alanı Genişlik * Yükseklik olarak hesaplanır (kare olduğu için genişlik ve yükseklik aynıdır)
+        public override double AlanHesapla()
+        {
+            return Genislik * Genislik;
+        }
+    }
+
+    public class Dikdortgen : BaseGeometrikSekil
+    {
+        // Dikdörtgenin alanı Genişlik * Yükseklik olarak hesaplanır
+        public override double AlanHesapla()
+        {
+            return Genislik * Yukseklik;
+        }
+    }
+
+    public class DikUcgen : BaseGeometrikSekil
+    {
+        // Dik üçgenin alanı (Genişlik * Yükseklik) / 2 olarak hesaplanır
+        public override double AlanHesapla()
+        {
+            return (Genislik * Yukseklik) / 2;
+        }
+    }
+
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApp5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Kare nesnesi oluşturma ve alan hesaplama
+        Kare kare = new Kare();
+        kare.Genislik = 5;
+        Console.WriteLine("Karenin Alanı: " + kare.AlanHesapla());
+
+        // Dikdörtgen nesnesi oluşturma ve alan hesaplama
+        Dikdortgen dikdortgen = new Dikdortgen();
+        dikdortgen.Genislik = 4;
+        dikdortgen.Yukseklik = 6;
+        Console.WriteLine("Dikdörtgenin Alanı: " + dikdortgen.AlanHesapla());
+
+        // Dik üçgen nesnesi oluşturma ve alan hesaplama
+        DikUcgen dikUcgen = new DikUcgen();
+        dikUcgen.Genislik = 3;
+        dikUcgen.Yukseklik = 4;
+        Console.WriteLine("Dik Üçgenin Alanı: " + dikUcgen.AlanHesapla());
+    }
+}
